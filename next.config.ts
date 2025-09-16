7b2c73265bea02eaf38966dc1efc7c5eaddd6203
Juan Delgado <josedelgadozambrano30@gmail.com>
2025-09-16 17:30:32 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 import type {NextConfig} from 'next';
-import withPWAInit from '@ducanh2912/next-pwa';
+
+const withPWAInit = require("@ducanh2912/next-pwa");
 
 const withPWA = withPWAInit({
   dest: 'public',
